c9b5c720c6df937e211bc208994611bb621cac1b
Asmik <jasmin_ayr@mail.ru>
2024-04-03 15:47:43 +0200

    EmployeeList added Employee and EmployeeController updated

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -5,10 +5,13 @@ import com.cydeo.model.Employee;
 import com.cydeo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -28,9 +31,21 @@ public class EmployeeController {
     }
 
     @PostMapping("/insert")
-    public String insertEmployee(@ModelAttribute("employee") Employee employee){
+    public String insertEmployee(@ModelAttribute("employee") @Valid Employee employee, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("stateList", DataGenerator.getAllStates());
+            return "employee/employee-create";
+        }
+
         employeeService.saveEmployee(employee);
-        return "employee/employee-list";
+        return "redirect:/employee/list"; //With redirect we are using endpoints
+
+    }
+
+    @GetMapping("/list")
+    public String listEmployee(Model model){
+        model.addAttribute("employeeList", employeeService.readAllEmployees());
+        return "employee/employee-list";//without redirect we are using html file paths
 
     }
 
--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -3,6 +3,8 @@ package com.cydeo.model;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
@@ -12,7 +14,19 @@ import java.time.LocalDate;
 @ToString
 
 public class Employee {
-    private String firstName,lastName, email, password, address, address2, city, state, zipcode;
+
+    //@NotNull   ----> Field shouldn't be null
+    //@NotEmpty  ----> Field shouldn't be ""
+    //@NotBlank  ----> Field shouldn't be "        "
+    //@NotNull   ----> @NotNull
+    //@NotEmpty  ----> @NotNull + @NotEmpty
+    //@NotBlank  ----> @NotNull + @NotEmpty +@NotBlanc
+    @NotBlank
+    @Size(max = 12, min = 2)
+    private String firstName,lastName;
+    private String email, password, address, address2, city, state, zipcode;
+
+    //Thymeleaf accepts yyyy-MM-dd, nut LocalDate accepts mm-dd-yyyy
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 
