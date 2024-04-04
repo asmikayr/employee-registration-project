cc9cfac748a8d8238c1b77e300ea41c5464fdd5a
Asmik <jasmin_ayr@mail.ru>
2024-04-04 11:45:43 +0200

    Valida

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -4,6 +4,8 @@ import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
@@ -24,9 +26,18 @@ public class Employee {
     @NotBlank
     @Size(max = 12, min = 2)
     private String firstName,lastName;
-    private String email, password, address, address2, city, state, zipcode;
+
+  //  @NotBlank
+    private String email;
+
+//    @NotBlank
+//    @Pattern(regexp="(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
+    private String password;
+
+    private String address, address2, city, state, zipcode;
 
     //Thymeleaf accepts yyyy-MM-dd, nut LocalDate accepts mm-dd-yyyy
+    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 
